f3c8bee7f096f834b123197b3bd547997780ffab
XuanThang2004 <nguyenxuanthang12012004@gmail.com>
2025-09-23 00:33:20 +0700

    update: modify gitignore, and add unit of work

---
+++ AGD.Repositories/Repositories/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using AGD.Repositories.Helpers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGD.Repositories.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {       
+        JwtHelper JwtHelper { get; }
+        Task<int> SaveChangesAsync();
+    }
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        //private readonly DbContext _context;
+        private readonly JwtHelper _jwtHelper;
+
+        public UnitOfWork(JwtHelper jwtHelper)
+        {
+            _jwtHelper = jwtHelper;
+        }
+
+        public JwtHelper JwtHelper => _jwtHelper;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AGD.Repositories/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using AGD.DAL.Basic;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGD.Repositories.Repositories
+{
+    //public class UserRepository : GenericRepository<User>
+    //{
+    //    private new readonly Dbcontext _context;
+    //    public UserRepository() => _context ??= new Dbcontext();
+    //    public UserRepository(Dbcontext context)
+    //    {
+    //        _context = context;
+    //    }
+
+    //    public async Task<User?> GetByUsernameAsync(string username)
+    //    {
+    //        return await _context.Users.FirstOrDefaultAsync(u => u.Username == username && !u.IsDeleted);
+    //    }
+
+    //    public async Task<User?> GetByEmailAsync(string email)
+    //    {
+    //        return await _context.Users.FirstOrDefaultAsync(u => u.Email == email && !u.IsDeleted);
+    //    }
+    //}
+}
